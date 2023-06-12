eb3b5695384717ebce071cb789dedda48c7aa78a
vitkondz <vitanhv26@gmail.com>
2023-06-12 15:38:20 +0700

    page not found

--- front-end/src/views/pages/PageNotFound.js
+++ front-end/src/views/pages/PageNotFound.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button, Container, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { useNavigate } from 'react-router-dom';
 
-function PageNotFound() {
+export default function PageNotFound() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Page not found</h1>
-    </div>
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh'
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Typography variant="h1">
+              Oops!
+            </Typography>
+            <Typography variant="h6">
+              The page you’re looking for doesn’t exist.
+            </Typography>
+            <Button variant="contained" color="info" onClick={() => navigate(-1)}>Back</Button>
+          </Grid>
+          <Grid xs={6}>
+            <img
+              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+              alt=""
+              width={500} height={250}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
-
-export default PageNotFound
